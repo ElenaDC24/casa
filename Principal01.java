3bb4dbdabc32f63842fc10cc8833f5290cde5d2d
ElenaDC24 <edocov.781@gmail.com>
2024-12-28 13:32:25 +0100

    Add files via upload

---
+++ Principal01.java
@@ -0,0 +1,11 @@
+package com.arquitecturajava.basicos;
+
+public class Principal01 {
+
+	public static void main(String[] args) {
+		//es un mensaje que sale por la consola
+		System.out.println("hola mundo desde java");
+
+	}
+
+}
