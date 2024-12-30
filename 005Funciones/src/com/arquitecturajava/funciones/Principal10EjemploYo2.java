416772a2b959c477df334f906f24960f5662548e
Elena <edocov.781@gmail.com>
2024-12-30 16:29:05 +0100

    prueba

---
+++ 005Funciones/src/com/arquitecturajava/funciones/Principal10EjemploYo2.java
@@ -0,0 +1,12 @@
+package com.arquitecturajava.funciones;
+
+public class Principal10EjemploYo2 {
+
+	public static void main(String[] args) {
+		
+		int radio= 3;
+		double area= Math.PI*radio*radio;
+
+	}
+
+}
